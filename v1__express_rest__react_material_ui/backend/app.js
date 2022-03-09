4cfc780a41e4440172443a1c90e1fe9a39b8e047
OptimistLabyrinth <keesung0705@gmail.com>
2022-03-09 23:14:12 +0900

    setup v1 express rest backend
    prettier + eslint
    express.js backend server

---
+++ v1__express_rest__react_material_ui/backend/app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+
+import { default as indexRouter } from './routes/index.js'
+import { default as usersRouter } from './routes/users.js'
+import dirName from './modules/dir-name.js'
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(dirName(), 'public')))
+
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+
+export default app
---
+++ v1__express_rest__react_material_ui/backend/modules/dir-name.js
@@ -0,0 +1,7 @@
+import path from 'path'
+
+function dirName() {
+  return path.resolve('.')
+}
+
+export default dirName
