4f5ad9b17db774583909cc64f2e2cb2876e4409c
OptimistLabyrinth <keesung0705@gmail.com>
2022-03-12 13:01:49 +0900

    create unit test for '/people' url router

---
+++ v1__express_rest__react_material_ui/backend/test/unit-test/routes/people.spec.js
@@ -0,0 +1,27 @@
+import { jest } from '@jest/globals'
+import { controllersForTest as peopleController } from '../../../routes/people.js'
+
+describe('test router/people.js functions', () => {
+  test('home : make sure the function exists', () => {
+    expect(peopleController.home).toBeDefined()
+  })
+
+  test('home : make sure res.send is called', async () => {
+    const req = {}
+    const res = {
+      send: jest.fn(() => {}),
+    }
+    await peopleController.home(req, res)
+    expect(res.send).toHaveBeenCalled()
+  })
+
+  // eslint-disable-next-line
+  test("home : res.send function sends object including 'msg' field", async () => {
+    const req = {}
+    const res = {
+      send: jest.fn(() => {}),
+    }
+    await peopleController.home(req, res)
+    expect(res.send.mock.calls[0][0]).toHaveProperty('msg')
+  })
+})
