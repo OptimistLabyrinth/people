302341a2a8a2c893d4e9d6566c974c6eeaf2dd81
OptimistLabyrinth <keesung0705@gmail.com>
2022-03-12 13:40:41 +0900

    readAll: unit test
    1) contains 'result' field
    2) 'result' field should be an array
    3) each person object in 'result' should have certain properties

--- v1__express_rest__react_material_ui/backend/routes/people.js
+++ v1__express_rest__react_material_ui/backend/routes/people.js
@@ -1,43 +1,43 @@
 import express from 'express'
 const router = express.Router()
 
-const examplePeople = [
-  {
-    id: 1,
-    name: 'John Deo',
-    nickname: 'john1234',
-    country: 'America',
-    birthday: '1990-10-20',
-    age: 31,
-    height: 177,
-    weight: 68,
-  },
-  {
-    id: 2,
-    name: 'Jane Doe',
-    nickname: 'janeeeee',
-    country: 'USA',
-    birthday: '1995-07-16',
-    age: 26,
-    height: 164,
-    weight: 50,
-  },
-  {
-    id: 3,
-    name: 'Tom Hiddleston',
-    nickname: 'tomExcvda',
-    country: 'UK',
-    birthday: '2000-03-25',
-    age: 21,
-    height: 180,
-    weight: 76,
-  },
-]
-
 router.get('/', readAll)
 
 function readAll(req, res) {
-  res.send({ msg: 'people router index' })
+  const examplePeople = [
+    {
+      id: 1,
+      name: 'John Deo',
+      nickname: 'john1234',
+      country: 'America',
+      birthday: '1990-10-20',
+      age: 31,
+      height: 177,
+      weight: 68,
+    },
+    {
+      id: 2,
+      name: 'Jane Doe',
+      nickname: 'janeeeee',
+      country: 'USA',
+      birthday: '1995-07-16',
+      age: 26,
+      height: 164,
+      weight: 50,
+    },
+    {
+      id: 3,
+      name: 'Tom Hiddleston',
+      nickname: 'tomExcvda',
+      country: 'UK',
+      birthday: '2000-03-25',
+      age: 21,
+      height: 180,
+      weight: 76,
+    },
+  ]
+
+  res.send({ msg: 'people router index', result: examplePeople })
 }
 
 const controllersForTest = {
--- v1__express_rest__react_material_ui/backend/test/unit-test/routes/people.spec.js
+++ v1__express_rest__react_material_ui/backend/test/unit-test/routes/people.spec.js
@@ -1,27 +1,71 @@
 import { jest } from '@jest/globals'
 import { controllersForTest as peopleController } from '../../../routes/people.js'
 
-describe('test router/people.js functions', () => {
-  test('home : make sure the function exists', () => {
-    expect(peopleController.readAll).toBeDefined()
-  })
+describe('test controllers in routes/people.js', () => {
+  describe('controller readAll', () => {
+    test('make sure the function exists', () => {
+      expect(peopleController.readAll).toBeDefined()
+    })
 
-  test('home : make sure res.send is called', async () => {
-    const req = {}
-    const res = {
-      send: jest.fn(() => {}),
-    }
-    await peopleController.readAll(req, res)
-    expect(res.send).toHaveBeenCalled()
-  })
+    test('make sure res.send is called inside the function', async () => {
+      const req = {}
+      const res = {
+        send: jest.fn(() => {}),
+      }
+      await peopleController.readAll(req, res)
+      expect(res.send).toHaveBeenCalled()
+    })
 
-  // eslint-disable-next-line
-  test("home : res.send function sends object including 'msg' field", async () => {
-    const req = {}
-    const res = {
-      send: jest.fn(() => {}),
-    }
-    await peopleController.readAll(req, res)
-    expect(res.send.mock.calls[0][0]).toHaveProperty('msg')
+    // eslint-disable-next-line
+    test("sends object including 'msg' field", async () => {
+      const req = {}
+      const res = {
+        send: jest.fn(() => {}),
+      }
+      await peopleController.readAll(req, res)
+      expect(res.send.mock.calls[0][0]).toHaveProperty('msg')
+    })
+
+    test('on success, return data with "result" field', async () => {
+      const req = {}
+      const res = {
+        send: jest.fn(() => {}),
+      }
+      await peopleController.readAll(req, res)
+      expect(res.send.mock.calls[0][0]).toHaveProperty('result')
+    })
+
+    test('on success, "result" field contains array of zero-to-many person', async () => {
+      const req = {}
+      const res = {
+        send: jest.fn(() => {}),
+      }
+      await peopleController.readAll(req, res)
+      expect(res.send.mock.calls[0][0]['result'].length).toBeGreaterThanOrEqual(
+        0
+      )
+    })
+
+    test(
+      'on success, each person in "result" array contains ' +
+        'id, name, nickname, country, birthday, age, height, weight',
+      async () => {
+        const req = {}
+        const res = {
+          send: jest.fn(() => {}),
+        }
+        await peopleController.readAll(req, res)
+        for (const eachPerson of res.send.mock.calls[0][0]['result']) {
+          expect(eachPerson).toHaveProperty('id')
+          expect(eachPerson).toHaveProperty('name')
+          expect(eachPerson).toHaveProperty('nickname')
+          expect(eachPerson).toHaveProperty('country')
+          expect(eachPerson).toHaveProperty('birthday')
+          expect(eachPerson).toHaveProperty('age')
+          expect(eachPerson).toHaveProperty('height')
+          expect(eachPerson).toHaveProperty('weight')
+        }
+      }
+    )
   })
 })
